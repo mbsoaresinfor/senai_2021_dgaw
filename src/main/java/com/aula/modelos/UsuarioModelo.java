6bdc3ffae6abe9371ad9f2705c028cb2d8186349
Marcelo Barbosa Soares <marcelo.soares@cwi.com.br>
2021-05-27 20:43:57 -0300

    aula 14 parte 2

--- src/main/java/com/aula/modelos/UsuarioModelo.java
+++ src/main/java/com/aula/modelos/UsuarioModelo.java
@@ -5,4 +5,18 @@ public class UsuarioModelo {
 	public Integer id;
 	public String login;
 	public String email;
+
+// MAPEAMENTO OBJETO PARA JSON , VICE-VERSA.
+//	UsuarioModelo modelo = new UsuarioModelo();
+//	modelo.id = 10;
+//	modelo.login = "marcelo";
+//	modelo.email = "marcelo@gmail.com"
+//	
+//	{
+//		"id": 10,
+//		"login": "marcelo",
+//		"email": "marcelo@gmail.com"
+//		
+//	}
+	
 }
