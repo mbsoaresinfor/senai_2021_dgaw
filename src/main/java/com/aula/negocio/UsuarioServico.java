d87a59d882eacecb6372b6964d1f6b6cc823c660
Marcelo Barbosa Soares <marcelo.soares@cwi.com.br>
2021-05-27 21:40:06 -0300

    aula 14 parte 3

--- src/main/java/com/aula/negocio/UsuarioServico.java
+++ src/main/java/com/aula/negocio/UsuarioServico.java
@@ -9,20 +9,35 @@ import com.aula.modelos.UsuarioModelo;
 @Service
 public class UsuarioServico {
 
+	ArrayList<UsuarioModelo> lista = new ArrayList<>();
+	static int contadorId = 1;
+	
 	public Integer salvar(UsuarioModelo usuario) {
-		return -1;
+		usuario.id = contadorId++;
+		lista.add(usuario);
+		return usuario.id;
 	}
 	
 	public ArrayList<UsuarioModelo> listar(){
-		return null;
+		return lista;
 	}
 	
 	public UsuarioModelo buscar(Integer id) {
+		for(UsuarioModelo usuario: lista) {
+			if(usuario.id == id) {
+				return usuario;
+			}
+		}
 		return null;
 	}
 	
-	public void deletar(Integer id) {}
+	public void deletar(Integer id) {
+		lista.removeIf(obj -> obj.id == id);
+	}
 	
-	public void atualizar(UsuarioModelo usuario) {}
+	public void atualizar(UsuarioModelo usuario) {
+		lista.removeIf(obj -> obj.id == usuario.id);
+		lista.add(usuario);
+	}
 		
 }
